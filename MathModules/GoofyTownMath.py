d7a9a997dae60fa5f4bfccf327d99820e10f9279
Marteinn23 <marteinn23@ru.is>
2024-03-26 15:39:04 +0000

    Added a good baseplate

---
+++ MathModules/GoofyTownMath.py
@@ -0,0 +1,35 @@
+"""
+The finished product
+
+Functions:
+    Add(x,y)
+    Subtract(x,y)
+    Multiply(x,y)
+    Divide(x,y)
+    Modulus(x,y)
+    Quotient(x,y)
+    Exponent(x,y)
+"""
+
+#Nothing set in stone, :stars_in_eyes:
+
+def Add(x,y,GoofyTown=False):
+    return x+y
+
+def Subtract(x,y,GoofyTown=False):
+    return x-y
+
+def Multiply(x,y,GoofyTown=False):
+    return x*y
+
+def Divide(x,y,GoofyTown=False):
+    return x/y
+
+def Modulus(x,y,GoofyTown=False):
+    return x%y
+
+def Quotient(x,y,GoofyTown=False):
+    return x//y
+
+def Exponent(x,y,GoofyTown=False):
+    return x**y
