9461cc5a33810ca735870f28449ae9fc538dbd4b
Marteinn23 <marteinn23@ru.is>
2024-03-26 16:43:41 +0000

    louie patch1: implemented goofy add

--- MathModules/teitur_goofytown_math.py
+++ MathModules/teitur_goofytown_math.py
@@ -10,15 +10,19 @@ Functions:
     Quotient(x,y)
     Exponent(x,y)
 """
+"""Nothing set in stone, :stars_in_eyes:"""
 
-#Nothing set in stone, :stars_in_eyes:
+import random, tkinter # An included library with Python install. 
+from tkinter import messagebox as mb  
 
 def Add(x,y,GoofyTown=False) -> int:
     if not GoofyTown:
         return x+y
     else:
-        theresult = x^y
-        return theresult
+        guess = random.randint(x,(x+y)*2)
+        while(mb.askyesno('Uhhhhhhh', f'{x} + {y} = {guess} {"?"*random.randint(1,13)}') != True):
+            guess = random.randint(x,(x+y)*2)
+        return guess
 
 def Subtract(x,y,GoofyTown=False):
     return x-y
@@ -40,3 +44,4 @@ def Exponent(x,y,GoofyTown=False):
 
 if __name__ == "__main__":
     print("Testing, Testing, 123")
+    print(Add(7,1,True))
